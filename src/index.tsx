ee3f929e0262cf175008c60aa8fdba56dc636296
Jiri Szlachta <jisz@teleoffice.lan>
2024-09-03 00:12:33 +0200

    Add new functionality
    - enable multi(de)select
    - highlights selected item(s)
    - shows all the info on a card
    - shows selected names on top of the page
    - separate styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { boxersList } from "./boxers";
+import './css/boxers.css';
+
+const selectedItems: string[] = []
+
+const handleCardSelect = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, item: any) => {
+
+    const index = selectedItems.indexOf(item.boxer.name)
+    if(index === -1) {
+        selectedItems.push(item.boxer.name);
+        event.currentTarget.className += ' selected'
+    } else {
+        selectedItems.splice(index, 1);
+        event.currentTarget.className = 'card';
+    }
+
+    let selectedBoxerNames = ""
+    for (let name of selectedItems) {
+        selectedBoxerNames += "<span>" + name + "</span>"
+    }
+
+    let infoDiv = document.getElementById('info')
+    if (infoDiv) {
+        infoDiv.innerHTML = selectedBoxerNames
+    }
+}
 
 const App = ({ boxers }: { boxers: Record<string, string | number>[] }) => {
     return (
-        <div style={{display: 'flex', flexWrap: 'wrap' }}>
-            {boxers.map((boxer) => <div key={boxer.name} style={{ width: 200, margin: 10, padding: 10, border: '1px solid #bbb', background: '#FAFAFA', borderRadius: 3, lineHeight: '10px' }}>{boxer.name}</div>)}
-        </div>
-
+        <>      
+            <h1>Boxers</h1>
+            <div id='info'></div>
+            <div id='cards'>
+                {boxers.map((boxer) => <div key={boxer.name} className='card' onClick={(e) => {handleCardSelect(e, {boxer})}}>
+                    <h2>{boxer.name}</h2>
+                    <div className='header'>Division: {boxer.division}</div>
+                    <div className='row'>
+                        <div className='col header'>Fights:</div>
+                        <div className='col'>{boxer.fights}</div>
+                        <div className='col header'>Kos:</div>
+                        <div className='col'>{boxer.kos}</div>
+                    </div>
+                    <div className='row'>
+                        <div className='col header'>Win:</div>
+                        <div className='col'>{boxer.win}</div>
+                        <div className='col header'>Loss:</div>
+                        <div className='col'>{boxer.loss}</div>
+                    </div>
+                </div>)}
+            </div>
+        </>
     )
 }
 
